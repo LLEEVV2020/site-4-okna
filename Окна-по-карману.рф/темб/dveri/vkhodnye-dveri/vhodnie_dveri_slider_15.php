260a152bf7a7fee639440e84fb6a0e6180a8c838
Лев Милов <milov@ir-tech.ru>
2019-08-05 17:33:02 +0400

    add

---
+++ Окна-по-карману.рф/темб/dveri/vkhodnye-dveri/vhodnie_dveri_slider_15.php
@@ -0,0 +1,111 @@
+      <section class="b-karusel2 template slider_doors_wrapper slider_doors_wrapper2">
+            <div class="container">
+
+                <div class="h2">ЛУЧШИЕ МЕЖКОМНАТНЫЕ ДВЕРИ  </div>
+                
+                    <section class=" slider_doors_js2">
+                        
+                        
+                        <div >
+                            <div class="item-rewiews">
+                                <div class="box1">
+                                    <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-1.png">
+                                    
+                                    <div class="red-button">ВЫГОДНО  </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div >
+                            <div class="item-rewiews">
+                                <div class="box1">
+                                    <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-2.png">
+                                    
+                                    <div class="red-button">ВЫГОДНО  </div>
+                                </div>
+                               
+                            </div>
+                            
+                        </div>
+                        <div >
+                            <div class="item-rewiews">
+                                <div class="box1">
+                                    <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-3.png">
+                                    
+                                    <div class="red-button">ВЫГОДНО  </div>
+                                </div>
+                               
+                            </div>
+                            
+                        </div>
+                        <div >
+                            <div class="item-rewiews">
+                                <div class="box1">
+                                    <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-4.png">
+                                    
+                                    <div class="red-button">ВЫГОДНО  </div>
+                                </div>
+                               
+                            </div>
+                            
+                        </div>
+                        <div >
+                            <div class="item-rewiews">
+                                <div class="box1">
+                                    <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-5.png">
+                                    
+                                    <div class="red-button">ВЫГОДНО  </div>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                        <div >
+                                <div class="item-rewiews">
+                                    <div class="box1">
+                                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/big-doors-2.png">
+                                    
+                                        <div class="red-button">ВЫГОДНО  </div>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                    </section>
+                    <div class="flex">
+   
+                            <button data-toggle="modal" data-target="#kupitDeshevle" class="red-button" type="submit">КУПИТЬ ДВЕРИ</button>
+              
+                        </div>
+                </div>   
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+                <script type="text/javascript">
+                    // ОТЗЫВЫ
+                        $(document).ready(function(){
+                            $(".slider_doors_js2").slick({
+                                
+                                dots: false,
+                                arrows: true,
+                                infinite: true,
+                                slidesToShow: 5,
+                                slidesToScroll: 1,
+                                responsive: [
+                                    {
+                                    breakpoint: 1200,
+                                        settings: {
+                                            slidesToShow: 1,
+                                            slidesToScroll: 1
+                                        }
+                                    }
+                                ]
+                            });
+                        });
+            
+                        
+                      </script>
+                      <style>
+                          .slider_doors_js2 .item-rewiews{
+                              padding-bottom: 62px;
+                          }
+                      </style>
+        </section> 
---
+++ Окна-по-карману.рф/темб/gde_kupit_shkafy_3/b_radar.php
@@ -0,0 +1,21 @@
+<div class="b-radar">
+   <!-- <div class="b-radar_bg" style="background: url(//<?php echo $_SERVER['SERVER_NAME']; ?>/city_include/<?php echo get_option('my_city_lat'); ?>/radar.jpg) no-repeat center center;">
+        <div class="b-radar_active">
+        </div>
+    </div>-->
+    <div class="b-radar_bg" style="background: url(//<?php echo $_SERVER['SERVER_NAME']; ?>/img//radar1.png) no-repeat center center;">
+        <div class="b-radar_active">
+        </div>
+    </div>
+</div>
+<!-- /.b-radar -->
+<style>
+.b-radar .b-radar_bg{
+    -webkit-transform: scale(0.65);
+    transform: scale(0.65);
+}
+.b-radar {
+    overflow: visible;
+    margin: 83px auto;
+}
+</style>
