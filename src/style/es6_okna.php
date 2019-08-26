84916bddc5a2203910471fee3582ef2548f382a9
Лев Милов <milov@ir-tech.ru>
2019-08-26 18:12:18 +0400

    add

--- src/style/es6_okna.php
+++
@@ -1,8 +0,0 @@
-<!--es6-->
-<div class="template price_2018">
-
-    <div class="container">
-        <div class="h2">ЦЕНЫ НА ОКНА В ЛУЧШИХ КОМПАНИЯХ 2018 ГОДА</div>
-        <img style="max-width: 100%;" src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/price_2018.png" alt="">
-    </div> 
-</div>
